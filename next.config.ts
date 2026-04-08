e24f914efcbb86df9759a234684076a1b2067480
Yam Yee Zhe <haojiao0803@gmail.com>
2026-04-08 22:34:29 +0800

    basepath in nextConfig is set to empty string

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,11 @@ const isGithubActions = process.env.GITHUB_ACTIONS === "true";
 const githubRepository = process.env.GITHUB_REPOSITORY ?? "";
 const repositoryName = githubRepository.split("/")[1] ?? "";
 const isUserOrOrgSite = repositoryName.endsWith(".github.io");
-const basePath = isGithubActions && !isUserOrOrgSite ? `/${repositoryName}` : "";
 
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath,
+  basePath: "",
   images: {
     unoptimized: true,
   },
